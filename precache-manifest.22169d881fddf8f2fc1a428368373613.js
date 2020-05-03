90025c4c8afa59ddeef5c6d501905ec1f2bfe01d
aurghyadip <adkundu@gmail.com>
2020-05-04 00:00:24 +0530

    Updates

---
+++ precache-manifest.22169d881fddf8f2fc1a428368373613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd1cde1362bc26297d7cf4540c5b0f5",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "18de773e791d338d7ee9",
+    "url": "/covid-19-tracker/static/css/main.08ab111e.chunk.css"
+  },
+  {
+    "revision": "568fb55c10cdc98acd37",
+    "url": "/covid-19-tracker/static/js/2.aa424b26.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.aa424b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18de773e791d338d7ee9",
+    "url": "/covid-19-tracker/static/js/main.1344bc16.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
